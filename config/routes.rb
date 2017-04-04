f1849ba7d702176cf2965d67e3c90289bb8c1396
Peter Jang <peter.x.jang@gmail.com>
2017-04-04 18:53:06 -0500

    Add employees API destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post "/employees" => "employees#create"
   get "/employees/:id" => "employees#show"
   patch "/employees/:id" => "employees#update"
+  delete "/employees/:id" => "employees#destroy"
 end
