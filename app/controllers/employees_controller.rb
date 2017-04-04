4d17589e72e743b2336ea4cc434dcd9ecb2a0aab
Peter Jang <peter.x.jang@gmail.com>
2017-04-03 21:18:28 -0500

    Add employees API create action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,18 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
     render "index.json.jbuilder"
   end
+  
+  def create
+    @employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birthdate: params[:birthdate],
+      email: params[:email],
+      ssn: params[:ssn]
+    )
+    @employee.save
+    redirect_to "/employees/#{@employee.id}"
+  end
 
   def show
     @employee = Employee.find_by(id: params[:id])
