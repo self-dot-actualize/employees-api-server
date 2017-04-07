f4266b0c12137cd76340793a8ea49a025cf0b8aa
Peter Jang <peter.x.jang@gmail.com>
2017-04-06 20:57:37 -0500

    Add user model and secure API with email and api_key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :restrict_access
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, options|
+      User.find_by(email: request.headers['X-User-Email'], api_key: api_key)
+    end
+  end
 end
